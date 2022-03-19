a5e404886c6392e477da72b4b1de7c85ea7d9b6b
Juana-89 <juana.lc@hotmail.es>
2022-03-19 16:56:45 -0500

    modificando los filtros

--- src/main.js
+++ src/main.js
@@ -73,7 +73,7 @@ filterOrderAZ.addEventListener("change", () => {
     }
         return 0;
     })
-    //console.log(filmsOrder);
+//console.log(filmsOrderAsc);
 showInfoMovies(filmsOrderAsc);   
 })
 //Filtrado descendente
@@ -89,7 +89,8 @@ filterOrderZA.addEventListener("change", () => {
     }
         return 0;
     })
-    //console.log(filmsOrderDes);
+
+//console.log(filmsOrderDesc);
 showInfoMovies(filmsOrderDesc);   
 })
 
@@ -127,13 +128,13 @@ filternameProducer.addEventListener("change", () => {
     showInfoMovies(filterMoviexProducer);
 });
 
-//hacer filtro del top 10 y top 5
+//hacer filtro de las movies mejores rankeadas
 const filterMoviesTop10 = document.querySelector("#input_publication3");
 filterMoviesTop10.addEventListener("change", () => {
-    
     const filterMoviexTop10 = films.sort(function(c,d){
-        if (filterMoviesTop10.value === "top10"){
+        if (filterMoviesTop10.value === "Mejores rankeadas"){
             if (parseInt(c.rt_score) < parseInt(d.rt_score)) {
+                
                 return 1;
             } if (parseInt(c.rt_score) > parseInt(d.rt_score)) {
                 return -1;
@@ -141,22 +142,18 @@ filterMoviesTop10.addEventListener("change", () => {
         }
             return 0;
         })
-        let top10 = filterMoviexTop10.slice(0,10);
-        console.log(top10);
-        showInfoMovies(top10);
-       /* console.log(filterMoviexTop10.slice(0,10));
-        showInfoMovies(filterMoviexTop10.slice(0,10));   */
+     
+    //console.log(filterMoviexTop10.slice(0,5));
+    showInfoMovies(filterMoviexTop10.slice(0,5));  
     });
-/////falta que solo se filtre 5
 
-    //hacer filtro del top 10 y top 5
 
-  const filterMoviesTop5 = document.querySelector("#input_publication3");
-
+//hacer filtro de las movies peores rankeadas
+ const filterMoviesTop5 = document.querySelector("#input_publication3");
 filterMoviesTop5.addEventListener("change", () => {
    
     const filterMoviexTop5 = films.sort(function(e,f){
-        if (filterMoviesTop5.value === "top5"){
+        if (filterMoviesTop5.value === "Menos rankeadas"){
       
             if (parseInt(e.rt_score) > parseInt(f.rt_score)) {
                 return 1;
@@ -164,10 +161,72 @@ filterMoviesTop5.addEventListener("change", () => {
                 return -1;
             }
         }
-            return 0;
-            
+            return 0;  
         })
-        let top5 = filterMoviexTop5.slice(0,5);
-        console.log(top5);
-        showInfoMovies(top5);
+       //console.log(filterMoviexTop5.slice(0,5));
+        showInfoMovies(filterMoviexTop5.slice(0,5));
     });
+
+
+//ver el detalle de las películas
+/*const detailDescriptionMovie = document.querySelector(".div_img_movie");
+detailDescriptionMovie.addEventListener("click", showMovieDetail(), true);
+function showMovieDetail (movie_detail){
+    mainmovies.innerHTML = "";
+    for (let index = 0; index < movie_detail.length; index++) {
+        let divUnitDescripMovie = `
+        div id="contenido-peliculas1"><br><br><br><br><br><br><br>
+<div class="contenedor_movies1">
+<a href=""><img src=${movie_detail[index].poster} alt="" class="div_img_movie1" /></a><br>
+<b><h3 class="contenedor_section_h3__movti1"> ${movie_detail[index].title}</h3></b> </br>
+<div class="div_contenido_movies1"><b>Fecha de publicación: </b>${movie_detail[index].release_date} </br>
+ <b>Director: </b>${movie_detail[index].director} </br>
+ <b>Productor: </b>${movie_detail[index].producer} </br>
+
+ </div>
+ </div>
+ </section>
+        `
+console.log(divUnitDescripMovie)
+mainmovies.innerHTML += divUnitDescripMovie;
+
+}
+
+});
+
+*/
+   
+//ver el detalle de las películas
+/*function showInformationMovies(film, array){
+    document.querySelector("#container_main_descriptionmovie").style.display = "none";
+    let filmInformation = "";
+  
+    for (let i = 0; i<film.length; i++ ){
+        film[i].addEventListener("click", () => {
+            document.querySelector("#container_main_descriptionmovie").style.display = "block";
+            for (let g = 0; g < array.length; g++){
+                if (i == g) {
+                    
+                 let filmInfoDiv =   `
+                 
+                    
+                    
+                    <a href=""><img src=${array[g].poster} alt="" class="div_img_movie" /></a><br>
+                    <b><h3 class="contenedor_section_h3__movti"> ${array[g].title}</h3></b> </br>
+                    <div class="div_detail_movies"><b>Fecha de publicación: </b>${array[g].description} </br>
+                     <b>Director: </b>${array[g].director} </br>
+                     <b>Productor: </b>${array[g].producer} </br>
+                     <b>Puntuación: </b>${array[g].rt_score}</br>
+                     </div>
+                     </div>`;
+                     filmInformation = filmInfoDiv;
+                }
+                mainmovies.innerHTML = filmInformation;
+            }
+        });
+    }
+
+}
+//console.log(showInformationMovies(films));
+showInformationMovies(films);
+*/
