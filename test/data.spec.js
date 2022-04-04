10328a43f1df865342d93ea6767fedcf7e21d6fb
cfape <c.penagos16@gmail.com>
2022-04-04 16:19:26 -0500

    arreglando estilo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,6 @@
 import { describe, it } from 'eslint/lib/rule-tester/rule-tester';
 import { filterMoviexProducer, filterYearPublished, sortMovieRanking, sortData } from '../src/data.js';
-// import data from '.src/data/ghibli/ghibli.js';
+
 
 const dataFilms =  [
   {"id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
@@ -49,30 +49,8 @@ describe('filterYearPublished', () => {
 });
 
 describe('sortMovieRanking', () => {
-  it('Devuelve el orden del puntaje de las película', () => {
-    const result = sortData(dataFilms, "93")
-    const resultExpected = [
-      {
-        id: '58611129-2dbc-4a81-a72f-77ddfc1b1b49',
-        title: 'Mi vecino Totoro',
-        director: 'Hayao Miyazaki',
-        producer: 'Hayao Miyazaki',
-        release_date: '1988',
-        rt_score: '93'
-      },
-      {
-        id: '2baf70d1-42bb-4437-b551-e5fed5a87abe',
-        title: 'El castillo en el cielo',
-        director: 'Hayao Miyazaki',
-        producer: 'Isao Takahata',
-        release_date: '1986',
-        rt_score: '95'
-      }
-    ]
-    expect(result).toEqual(resultExpected);
-    });
     it('Devuelve el orden del puntaje de las película', () => {
-      const result = sortData(dataFilms, "Menos rankeadas")
+      const result = sortMovieRanking(dataFilms, "Menos rankeadas")
       const resultExpected = [
         {
           id: '58611129-2dbc-4a81-a72f-77ddfc1b1b49',
@@ -93,7 +71,7 @@ describe('sortMovieRanking', () => {
       ]
       expect(result).toEqual(resultExpected);
       });
-});
+  });
 
 describe('sortData', () => {
   it('Devuelve el orden de la película A-Z', () => {
