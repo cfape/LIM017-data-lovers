9488c09ad34ea725a8c6da744d31286b4eca874f
cfape <c.penagos16@gmail.com>
2022-04-04 11:37:25 -0500

    agregando test

--- test/data.spec.js
+++ test/data.spec.js
@@ -69,13 +69,49 @@ describe('sortData', () => {
     const result = sortData(dataFilms, "a.title < b.title");
     expect(result).toBeTruthy();
   });
-  it('Devuelve el orden de la película Z-A', () => {
-    const result = sortData(dataFilms, "Mi vecino Totoro")
-    expect(result).toBeTruthy();
+  it('Devuelve el orden de la película A-Z', () => {
+    const result = sortData(dataFilms, "A-Z")
+    const resultExpected = [
+      {
+        id: '2baf70d1-42bb-4437-b551-e5fed5a87abe',
+        title: 'El castillo en el cielo',
+        director: 'Hayao Miyazaki',
+        producer: 'Isao Takahata',
+        release_date: '1986',
+        rt_score: '95'
+      },
+      {
+        id: '58611129-2dbc-4a81-a72f-77ddfc1b1b49',
+        title: 'Mi vecino Totoro',
+        director: 'Hayao Miyazaki',
+        producer: 'Hayao Miyazaki',
+        release_date: '1988',
+        rt_score: '93'
+      }
+    ]
+    expect(result).toEqual(resultExpected);
   });
-  it('Devuelve el orden de la película de A-Z', () => {
-    const result = sortData(dataFilms, "Mi vecino Totoro")
-    expect(result).toBeTruthy();
+  it('Devuelve el orden de la película de Z-A', () => {
+    const result = sortData(dataFilms, "Z-A")
+    const resultExpected = [
+      {
+        id: '58611129-2dbc-4a81-a72f-77ddfc1b1b49',
+        title: 'Mi vecino Totoro',
+        director: 'Hayao Miyazaki',
+        producer: 'Hayao Miyazaki',
+        release_date: '1988',
+        rt_score: '93'
+      },
+      {
+        id: '2baf70d1-42bb-4437-b551-e5fed5a87abe',
+        title: 'El castillo en el cielo',
+        director: 'Hayao Miyazaki',
+        producer: 'Isao Takahata',
+        release_date: '1986',
+        rt_score: '95'
+      }     
+    ]
+    expect(result).toEqual(resultExpected);
   });
   it('Es un resultado con dato definido', () => {
     expect(sortData).toBeDefined();
